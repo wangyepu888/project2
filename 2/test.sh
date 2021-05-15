0439a42b191f3bb224e91ab4b8c298bd40deaf2c
wangyepu888 <34576117+wangyepu888@users.noreply.github.com>
2021-05-15 08:32:57 +0800

    Update test.sh

--- 2/test.sh
+++ 2/test.sh
@@ -7,6 +7,6 @@ source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /census2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../test-data/train.csv /census1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../test-data/test.csv /census2/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./part3.py \
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./part4.py \
             hdfs://$SPARK_MASTER:9000/census1/input/ \
             hdfs://$SPARK_MASTER:9000/census2/input/ \
